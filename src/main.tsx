7a055ac84e68f350f792d506daf616f0a2983e14
Robert KANISSIDI <violethekrr@gmail.com>
2026-08-12 16:40:41 +0100

    Add main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import "./App.css";
-import App from "./App";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
